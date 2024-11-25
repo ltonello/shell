3ad83af52348e776ef986a2cae74549d580a5db9
lauren <lauren.tonello@gmail.com>
2024-11-24 17:22:45 -0800

    completed assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,12 +45,8 @@ cp raw/*user*.log processed/user_logs
 cp raw/*event*.log processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
 rm raw/*ipaddr*
 rm processed/user_logs/*ipaddr*
-=======
-rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls processed/event_logs > inventory.txt
